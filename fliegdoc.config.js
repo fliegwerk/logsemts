97a12ed9108b78e5c4f14eecfb89553b4640357f
Pablo Klaschka <contact@pabloklaschka.de>
2021-05-09 15:44:44 +0200

    docs: Switch to `fliegdoc` as documentation generation engine

---
+++ fliegdoc.config.js
@@ -0,0 +1,20 @@
+// Generated using fliegdoc init
+module.exports = {
+  title: '@fliegwerk/logsemts',
+  readme: './README.md',
+  outDir: './docs',
+  baseUrl: '/logsemts/',
+  hidePrivateMembers: true,
+  externalLinks: {
+    GitHub: 'https://github.com/fliegwerk/logsemts',
+    Homepage: 'https://www.fliegwerk.com/projects/logsemts',
+    'Medium Article': 'https://medium.com/@pklaschka/semantic-logging-in-js-and-ts-with-the-fliegwerk-logsemts-library-and-why-i-developed-it-d150dfae423f?source=friends_link&sk=4701990c91350ed6d7a81f7ced50133c'
+  },
+  modules: [
+    {
+      tsconfig: './tsconfig.json',
+      package: './package.json',
+      mainFile: 'index.ts'
+    }
+  ]
+}
